d6dd8e0361750bdb9b2a3f2bcdef3d5c5e042b1a
MelkerBernhardsson <mebe05001@utb.vaxjo.se>
2022-09-09 09:22:51 +0200

    Add project files.

---
+++ kap 1-3 uppgift4/kap_1_3_uppgift4.py
@@ -0,0 +1,7 @@
+
+pris=int(input("pris="))
+rabatt=float(input("rabatt="))   #rabatten maste skrivas som decimal
+summa=pris*rabatt
+print("gurkan kostar",pris)
+print("men det finns en",rabatt, "rabatt")
+print("gurkan kostar:",summa)
